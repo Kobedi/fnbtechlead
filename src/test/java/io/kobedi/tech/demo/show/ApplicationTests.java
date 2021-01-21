73a4ad06659c22f191c3c19321a2eea8c8ccaaf0
Kobedi Makgabutlane <makgadk2@gmail.com>
2021-01-21 20:46:00 +0200

    Added a registration class to hold registration details

---
+++ src/test/java/io/kobedi/tech/demo/show/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kobedi.tech.demo.show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/io/kobedi/tech/demo/show/domain/Registration.java
@@ -0,0 +1,73 @@
+package io.kobedi.tech.demo.show.domain;
+
+public class Registration {
+	
+	private long id;
+	private long seatNumber;
+	private String name;
+	private String surname;
+	private String email;
+	private String cellPhone;
+	private String dateCreated;
+	private String dateUpDated;
+	private String updatedBy;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getSeatNumber() {
+		return seatNumber;
+	}
+	public void setSeatNumber(long seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCellPhone() {
+		return cellPhone;
+	}
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public String getDateUpDated() {
+		return dateUpDated;
+	}
+	public void setDateUpDated(String dateUpDated) {
+		this.dateUpDated = dateUpDated;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	
+	
+	
+	
+
+}
